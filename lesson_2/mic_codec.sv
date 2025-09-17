859fa6f8f3279ca3a124b198065339d75408da24
Rafael Melo Macieira <rfamm01@gmail.com>
2025-09-17 13:54:30 +1000

    Lesson 2 with testbench

--- lesson_2/mic_codec.sv
+++ lesson_2/mic_codec.sv
@@ -1,22 +1,36 @@
 module mic_codec(input s_clk, input n_rst, input mic_clk, input mic_data, output logic left_mic_data, output logic right_mic_data);
 
 logic [4:0] counter;
+logic last_m_clk;
+
+localparam DELAY = 5'd2;
 
 always_ff @ (negedge n_rst or posedge s_clk) begin
 	if (!n_rst) begin
 		left_mic_data <= 1'b0;
 		right_mic_data <= 1'b0;
 		counter <= 5'd0;
+		last_m_clk <= 1'b0;
 	end else begin
-		if (counter >= 5'd5) begin
-			left_mic_data <= mic_clk ? mic_data : left_mic_data;
-			right_mic_data <= !mic_clk ? mic_data : right_mic_data;
-			counter <= 5'd0; 
-		end else begin
+		if (mic_clk ^~ last_m_clk) begin
 			left_mic_data <= left_mic_data;
 			right_mic_data <= right_mic_data;
-			counter <= counter + 5'd1;
+			counter <= 5'd0;
+			last_m_clk <= last_m_clk;
+		end else begin
+			if (counter >= DELAY) begin
+				left_mic_data <= mic_clk ? mic_data : left_mic_data;
+				right_mic_data <= !mic_clk ? mic_data : right_mic_data;
+				counter <= 5'd0;
+				last_m_clk <= ~last_m_clk;
+			end else begin
+				left_mic_data <= left_mic_data;
+				right_mic_data <= right_mic_data;
+				counter <= counter + 5'd1;
+				last_m_clk <= last_m_clk;
+			end			
 		end
+		
 	end
 end
 
--- lesson_2/testbench.sv
+++ lesson_2/testbench.sv
@@ -1,15 +1,14 @@
-`timescale 20ns / 10ps
+`timescale 10ns / 10ps
 
 module testbench();
 
 logic s_clk;
-logic s_n_rst; 
-logic led_out;
+logic m_clk;
+logic m_data;
+logic left_mic_data;
+logic right_mic_data;
 
-blink_led led_0(.clk(s_clk),
-		.n_rst(s_n_rst),
-		.led_out(led_out)
-		);
+logic s_n_rst; 
 
 initial begin
 	s_clk = 1'b1;
@@ -22,4 +21,30 @@ initial begin
 	s_n_rst = 1'b1;
 end
 
+initial begin
+	m_data = 1'b0;
+	forever begin
+		@(m_clk);
+		m_data = #2 $random % 2;
+	end
+end
+
+mic_pll pll_0 (
+		.s_clk(s_clk), //system clock input
+		.n_rst(s_n_rst), //reset input
+		.us_clk(m_clk) //ultrasonic mode clock output
+		//.std_clk(std_clk) //standard mode clock output
+		);
+					
+mic_codec codec_0 (
+		.s_clk(s_clk),
+		.n_rst(s_n_rst),
+		.mic_clk(m_clk),
+		.mic_data(m_data),
+		.left_mic_data(left_mic_data),
+		.right_mic_data(right_mic_data)
+		);
+
 endmodule
+
+
